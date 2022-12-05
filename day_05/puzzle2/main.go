311e8c892837a5bb4cd36c8defd93bdf96b57a1d
Mehdi Hasan Khan <mehdi.khan@contentful.com>
2022-12-05 17:27:27 +0100

    finish day 05

---
+++ day_05/puzzle2/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type stack []rune
+
+func (s *stack) push(r rune) {
+	*s = append(*s, r)
+}
+
+func (s *stack) pushN(r []rune) {
+	*s = append(*s, r...)
+}
+
+func (s *stack) pop() (r rune) {
+	r, *s = (*s)[len(*s)-1], (*s)[:len(*s)-1]
+	return
+}
+
+func (s *stack) popN(n int) (r []rune) {
+	r = (*s)[len(*s)-n : len(*s)]
+	*s = (*s)[:len(*s)-n]
+	return
+}
+
+func (s *stack) unshift(r rune) {
+	*s = append([]rune{r}, *s...)
+}
+
+func main() {
+	f, err := os.Open("../input_small.txt")
+	if err != nil {
+		panic(err)
+	}
+	sc := bufio.NewScanner(f)
+
+	var stacks []stack
+
+	for sc.Scan() {
+		line := sc.Text()
+
+		if len(line) < 1 {
+			continue
+		}
+
+		switch true {
+		case strings.HasPrefix(strings.TrimSpace(line), "["):
+			if len(stacks) <= 0 {
+				// init
+				stacks = make([]stack, (len(line)+1)/4)
+			}
+
+			for i, r := range line {
+				if r != ' ' && r != '[' && r != ']' {
+					stacks[i/4].unshift(r)
+				}
+			}
+		case strings.HasPrefix(line, "move"):
+			var move, from, to int
+			_, _ = fmt.Sscanf(sc.Text(), "move %d from %d to %d", &move, &from, &to)
+
+			stacks[to-1].pushN(stacks[from-1].popN(move))
+		default:
+			// ignore 1   2   3... line
+		}
+	}
+
+	for _, s := range stacks {
+		fmt.Print(string(s.pop()))
+	}
+}
